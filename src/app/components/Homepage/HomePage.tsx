4bcf363d52447a43122d2bcac4ae9d5efce4c1e5
addey-eweis <addeyeweisofficial@gmail.com>
2024-01-30 10:11:48 +0200

    Footer was almost deleted

--- src/app/components/Homepage/HomePage.tsx
+++ src/app/components/Homepage/HomePage.tsx
@@ -1,6 +1,6 @@
 import HomePageHeader from "@/app/components/Homepage/HomePageComponents/HomePageHeader";
 import FooterComponent from "@/app/components/Homepage/FooterComponent";
-import MadeByRollinBoat from "@/app/components/DeveloperCredit/MadeByRollinBoat";
+import MadeByRollinBoat from "@/app/components/Homepage/MadeByRollinBoat/MadeByRollinBoat";
 
 
 export default function HomePage() {
