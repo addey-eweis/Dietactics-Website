b9fc8de10591ed76e6f54171fe0e96347fad512c
addey-eweis <addeyeweisofficial@gmail.com>
2024-03-02 15:11:38 +0200

    Restructure

--- src/app/components/Homepage/SupplyChainIntelComponent/SupplyChainIntelComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from "./SupplyChainIntelComponent.module.scss"
-
-export default function SupplyChainIntelComponent() {
-    return (<>
-        <section className={styles.mainContainer}>
-        </section>
-    </>)
-}
